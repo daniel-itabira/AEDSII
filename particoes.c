e6fac9a49ecfb36075cef5943cc3ab89b2f58015
Daniel Rodrigues <57416717+daniel-itabira@users.noreply.github.com>
2021-04-02 16:48:40 -0300

    Seleção Natural - Falta acabar impressão

--- particoes.c
+++ particoes.c
@@ -7,7 +7,6 @@
 void classificacao_interna(FILE *arq, Lista *nome_arquivos_saida, int M, int nFunc) {
     rewind(arq); //posiciona cursor no inicio do arquivo
 
-
     int reg = 0;
 
     while (reg != nFunc) {
@@ -59,116 +58,202 @@ void classificacao_interna(FILE *arq, Lista *nome_arquivos_saida, int M, int nFu
     }
 }
 
-void selecao_natural(FILE *arq, Lista *nome_arquivos_saida, int M, int nFunc, int n){
-    /*PARTE 1 TRABALHO*/
+void selecao_natural(FILE *arq, Lista *nome_arquivos_saida, int M, int nFunc, int numeroNomes){
+    int totalLidos = 0;
+    Lista* nomes = nome_arquivos_saida;
+    TFunc** v = malloc(M * sizeof(TFunc*));
+    int noVetor = 0;
 
-    //posiciona cursor no inicio do arquivo
-    rewind(arq);
+    FILE* repositorio = fopen("repository.dat", "w+");
+    int noRepositorio = 0;
 
-    int reg = 0;
-    int i=0;
-    int controle = 0;
-    TFunc *v[M];
+    Lista* nomeAtual = nomes;
+    FILE* particaoAtual = NULL;
+    int naParticaoAtual = 0;
 
-    //Passo 1: (POSSIVEL ALTERAÇÃO NECESSÁRIA
-    //le o arquivo e colocar os dados em um vetor
-    //se controle == 0 é pra ler o arquivo inicial (arq)
-    if(controle==0){
-        while (reg != nFunc) {
-            while (!feof(arq)) {
-                fseek(arq, (reg) * tamanho_registro(), SEEK_SET);
-                v[i] = le_funcionario(arq);
-                i++;
-                reg++;
-                if(i>=M) break;
-                }
-            }
+    while(possoLerMais(totalLidos, nFunc)) {
+        if(noVetor < M) {
+            v[noVetor] = recuperaFuncionario(arq, &totalLidos);
+            noVetor++;
         }
 
-        //APARTIR DA SEGUNDA ITERAÇÃO
-        /*else {
-             //verificar o tamanho do reservatório (tam)
-             //criar um vetor TFunc *v[tam];
-             reg=0;
-             i=0;
+        if(noVetor < M)
+            continue;
 
-             while (reg != tam) {
-                    while (!feof(reservatorio)) {
-                        fseek(resevatorio, (reservatorio) * tamanho_registro(), SEEK_SET);
-                        v[i] = le_funcionario(reservatorio);
-                        i++;
-                        reg++;
-                        if(i>=tam) break;
-                        }
+        int menor = buscarMenor(v, noVetor);
+        if(particaoAtual == NULL) {
+            char* nome = nomes->nome;
+            particaoAtual = abrirParticao(nome);
+            nomeAtual = nomes;
+
+            atualizaNomeNaParticao(nomes, numeroNomes);
+            nomes = nomes->prox;
+        }
+        if(naParticaoAtual >= M) {
+            fclose(particaoAtual);
+            particaoAtual = NULL;
+            naParticaoAtual = 0;
+            continue;
+        }
+        fseek(particaoAtual, naParticaoAtual * tamanho_registro(), SEEK_SET);
+        salva_funcionario(v[menor], particaoAtual);
+
+        naParticaoAtual++;
+
+        if(!possoLerMais(totalLidos, nFunc)) {
+            for (int i = menor; i < noVetor - 1; i++) {
+                v[i] = v[i + 1];
             }
-
-        }*/
-
-    //Passo 2:
-    // Escolher o menor registro R
-    //verifica os códigos
-    int r=v[0]->cod;
-    //recebe a posição
-    int pos;
-
-        for(i=0;i<M;i++){
-                if(r>v[i]->cod){
-                    r=v[i]->cod;
-                    pos=i;
-                }
-                //imprime_funcionario(v[i]);
-                 //free(v[i]);
-                }
-
-
-    //Passo 3:
-    //Gravar o registro r na partição de saida
-
-
-     //cria arquivo de particao e faz gravacao
-        char *nome_particao = nome_arquivos_saida->nome;
-        nome_arquivos_saida = nome_arquivos_saida->prox;
-        printf("\n%s\n", nome_particao);
-        FILE *p;
-        if ((p = fopen(nome_particao, "wb+")) == NULL) {
-            printf("Erro criar arquivo de saida\n");
+            noVetor--;
+            break;
         }
 
-        //coloca o registro de menor valor [R] na partição criada
-        salva_funcionario(v[pos], p);
-        else {
-            //não pode ser M e sim o tamanho do vetor, vai dar pau na segunda iteração
-            //PASSO 4:
-            for (int i = 0; i < M; i++) {
+        int code = v[menor]->cod;
+        free(v[menor]);
+        v[menor] = recuperaFuncionario(arq, &totalLidos);
 
-                    //quando o i chegar na posição do elemento já adicionado pular
-                    if(i==pos){
-                        continue;
-                    }
+        menor = buscarMenor(v, noVetor);
 
-                    fseek(p, (i) * tamanho_registro(), SEEK_SET);
-                    //PASSO 5
-                    if(v[i]->cod <v[i-1]->cod){
-                      //colocar v[i] no reservatorio
-                      //salva_funcionario(v[i], p);
-                    } else {
-                        //colocar v[i] na partição de saida
-                        fseek(p, (i) * tamanho_registro(), SEEK_SET);
-                        salva_funcionario(v[i], p);
-                    }
-
-               /* fseek(p, (i) * tamanho_registro(), SEEK_SET);
-                salva_funcionario(v[i], p);
-                imprime_funcionario(v[i]);*/
+        if(v[menor]->cod < code) {
+            fseek(repositorio, noRepositorio * tamanho_registro(), SEEK_SET);
+            salva_funcionario(v[menor], repositorio);
+            noRepositorio++;
+            if(!possoLerMais(totalLidos, nFunc)) {
+                for (int i = menor; i < noVetor - 1; i++) {
+                    v[i] = v[i + 1];
+                }
+                noVetor--;
+                break;
             }
-            fclose(p);
+            v[menor] = recuperaFuncionario(arq, &totalLidos);
         }
 
-        //PASSO 7: PENSAR CONDIÇÕES
-        //PEGAR O RESERVATÓRIO LER SEUS DADOS NO VETOR V[] E REPETIR
-        //PRECISA-SE SABER O TAMANHO DO VETOR
+        if(noRepositorio < numeroNomes) {
+            continue;
+        }
+        fclose(particaoAtual);
+
+        char* nome = nomes->nome;
+        particaoAtual = abrirParticao(nome);
+        nomeAtual = nomes;
+        atualizaNomeNaParticao(nomes, numeroNomes);
+        nomes = nomes->prox;
+
+        int* ultimoSalvo = NULL;
+        int i = 0;
+        while(noVetor > 0)
+        {
+            fseek(particaoAtual, i * tamanho_registro(), SEEK_SET);
+            int lower = buscarMenor(v, noVetor);
+            salva_funcionario(v[lower], particaoAtual);
+            imprime_funcionario(v[i]);
+
+            for (int i = lower; i < noVetor - 1; i++) {
+                v[i] = v[i + 1];
+            }
+            noVetor--;
+            i++;
+        }
+
+        for (int i = 0; i < noVetor; i++)
+            free(v[i]);
+        noVetor = 0;
+        for (int i = 0; i < noRepositorio; i++)
+        {
+            fseek(repositorio, i * tamanho_registro(), SEEK_SET);
+            v[i] = le_funcionario(repositorio);
+            noVetor++;
+        }
+        noRepositorio = 0;
+
+        fclose(particaoAtual);
+        particaoAtual = NULL;
+        naParticaoAtual = 0;
 
 
+    }
 
-return 0;
+    if(particaoAtual) {
+        fclose(particaoAtual);
+        particaoAtual = NULL;
+        naParticaoAtual = 0;
+    }
+    int ultimaParticao = 0;
+    if(!(noVetor <= 0 && noRepositorio <= 0)) {
+
+        if(noRepositorio > 0) {
+            char* nome = nomes->nome;
+            particaoAtual = abrirParticao(nome);
+            nomeAtual = nomes;
+            naParticaoAtual = 0;
+            for (int i = 0; i < noRepositorio; i++)
+            {
+                fseek(repositorio, i * tamanho_registro(), SEEK_SET);
+                TFunc* func = le_funcionario(repositorio);
+                fseek(particaoAtual, naParticaoAtual * tamanho_registro(), SEEK_SET);
+                salva_funcionario(func, particaoAtual);
+                imprime_funcionario(v[i]);
+                naParticaoAtual++;
+            }
+            fclose(particaoAtual);
+            naParticaoAtual = 0;
+        }
+
+        if(noVetor > 0) {
+            atualizaNomeNaParticao(nomes, numeroNomes);
+            nomes = nomes->prox;
+            char* nome = nomes->nome;
+            particaoAtual = abrirParticao(nome);
+            nomeAtual = nomes;
+            naParticaoAtual = 0;
+            for (int i = 0; i < noVetor; i++)
+            {
+                fseek(particaoAtual, i * tamanho_registro(), SEEK_SET);
+                salva_funcionario(v[i], particaoAtual);
+                imprime_funcionario(v[i]);
+                naParticaoAtual++;
+            }
+            fclose(particaoAtual);
+        }
+    }
+    fclose(repositorio);
+    free(v);
+
 }
+
+TFunc* recuperaFuncionario(FILE* arq, int* totalLidos) {
+    fseek(arq, *totalLidos * tamanho_registro(), SEEK_SET);
+    TFunc* func = le_funcionario(arq);
+    (*totalLidos)++;
+    return func;
+}
+
+void atualizaNomeNaParticao(Lista* nomes, int* numeroNomes) {
+    if(nomes->prox == NULL) {
+        char* novoNome = malloc(5 * sizeof(char));
+        (*numeroNomes)++;
+        sprintf(novoNome, "p%d.dat", *numeroNomes);
+        nomes->prox = cria(novoNome, NULL);
+    }
+}
+
+int possoLerMais(int totalLidos, int nFunc) {
+    return totalLidos < nFunc;
+}
+
+FILE* abrirParticao(char* nome) {
+    FILE* file = fopen(nome, "w+");
+    return file;
+}
+
+int buscarMenor(TFunc* v[], int tam) {
+    int menor = 0;
+    for (int k = 1; k < tam; k++)
+    {
+        if(v[menor]->cod > v[k]->cod)
+            menor = k;
+    }
+    return menor;
+}
+
+
